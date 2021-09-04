98f73563abfe4666b777a1a124d944801a903f8a
Zeeshan Quamar <76791320+zishugshan@users.noreply.github.com>
2021-09-04 12:58:44 +0530

    Update Algorithms.cpp

--- Algorithms.cpp
+++ Algorithms.cpp
@@ -17,4 +17,144 @@
             }
         }
     }
+
+
+// day1 : 2>> https://leetcode.com/problems/first-bad-version/
+
+    int firstBadVersion(int n) {
+        int l = 1;
+        int h = n;
+        int badVersion = 0;
+        while(l<=h){
+            int bad = l + (h-l)/2;
+            if(isBadVersion(bad)){
+                badVersion = bad;
+                h = bad-1;
+            }
+            else{
+                l = bad+1;
+            }
+        }
+        return badVersion;
+    }
+
+
+// day1 : 3>> https://leetcode.com/problems/search-insert-position/
+
+    int searchInsert(vector<int>& nums, int target) {
+        int n = nums.size();
+        int l = 0;
+        int h = n-1;
+        int idx = -1;
+        while(l<=h){
+            int mid = h-(h-l)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            if(nums[mid]<target){
+                l = mid+1;
+                idx = mid+1;
+            }
+            else{
+                h = mid-1;
+                idx = mid;
+            }       
+        }
+        return idx;
+    }
+
+
+// day2 : 1>> https://leetcode.com/problems/squares-of-a-sorted-array/
+
+    vector<int> sortedSquares(vector<int>& nums) {
+        multimap<int,int>mm;
+        vector<int>v;
+        for(int i=0;i<nums.size();i++){
+            mm.insert({nums[i]*nums[i],nums[i]});
+        }
+        for(auto it = mm.begin(); it != mm.end();it++){
+            v.push_back(it->first);
+        }
+        return v;
+    }
+
+
+// day2 : 2>> https://leetcode.com/problems/rotate-array/
+
+    void rotate(vector<int>& nums, int kk) {
+        int n =nums.size();
+        int k = kk%n;
+        int arr[n-k];
+        for(int i=0;i<n-k;i++){
+            arr[i]=nums[i];
+        }
+        for(int i =0;i<k;i++){
+            nums[i]=nums[n-k+i];
+        }
+        for(int i=0;i<n-k;i++){
+            nums[k+i]=arr[i];
+        }
+    }
+
+
+// day3 : 1>> https://leetcode.com/problems/move-zeroes/
+
+    void moveZeroes(vector<int>& nums) {
+        int n = nums.size();
+        int j=0;
+        vector<int>arr(n,0);
+        for(int i=0;i<n;i++){
+            if(nums[i]!=0) arr[j++]=nums[i];
+            }
+        
+         for(int k=0;k<n;k++){
+            nums[k]=arr[k];
+        }
+        
+        }
+    
+
+// day3 : 2>> https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
+
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        unordered_map<int,int>m;
+        vector<int>v;
+        for(int i=0;i<numbers.size();i++){
+            m.insert({numbers[i],i});
+        }
+        
+        for(int i=0;i<numbers.size();i++){
+            if(numbers[i]==target/2){
+                v.push_back(i+1);
+                v.push_back(i+2);
+            }
+            else if((m[target-numbers[i]])>0){
+                v.push_back(i+1);
+                v.push_back(m[target-numbers[i]]+1);
+            }
+            if(!v.empty()) break;
+        }
+        return v;
+    }
+
+
+// day4 : 1>> https://leetcode.com/problems/reverse-string/
+
+    void reverseString(vector<char>& s) {
+        int l=0;
+        int h=s.size()-1;
+        while(l<h){
+            swap(s[l],s[h]);
+            l++;h--;
+        }
+        
+    }
+
+
+// day4 : 2>> https://leetcode.com/problems/reverse-words-in-a-string-iii/
+
+
+        
+        
+ 
    
